9cae0b959d43ba8db1e251632bcb81f70a96cdc1
sunfeilong1993 <1498282352@qq.com>
2018-04-14 06:07:25 +0800

    删除Sql

--- test/src/test.sql
+++
@@ -1,5 +0,0 @@
-select count(1) from wx_material_news where authorized_appid = 'wx7f900e8dbdf6b939' and with_link = 1 and status = 1
-
-
-select count(1) from wx_material_news wxn, wx_material_article wma where wxn.news_id = wma.material_news_id
-and wxn.authorized_appid = 'wx7f900e8dbdf6b939' and wxn.with_link = 1 and wxn.status = 1 and wma.status = 1;
