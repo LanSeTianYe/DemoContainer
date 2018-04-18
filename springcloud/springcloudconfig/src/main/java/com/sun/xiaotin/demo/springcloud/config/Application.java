19358cb09046428d2aa1aae1bd5198c16cc5785f
sunfeilong1993 <1498282352@qq.com>
2018-04-18 10:26:46 +0800

    Spring cloud 项目初始化

---
+++ springcloud/springcloudconfig/src/main/java/com/sun/xiaotin/demo/springcloud/config/Application.java
@@ -0,0 +1,27 @@
+package com.sun.xiaotin.demo.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+public class Application {
+
+    @Value("${config.name}")
+    String name = "World";
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
