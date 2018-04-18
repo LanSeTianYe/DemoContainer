853f4fc15929f2c121556009e10c2589e47cde2e
sunfeilong1993 <1498282352@qq.com>
2018-04-18 10:26:15 +0800

    Stream 测试

---
+++ test/src/main/java/com/sun/xiaotian/demo/test/TestStream.java
@@ -0,0 +1,14 @@
+package com.sun.xiaotian.demo.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestStream {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+    }
+    
+}
