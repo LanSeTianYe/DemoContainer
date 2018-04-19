34efa3386291210fd1a09b9a4dcfe70aca1e20ca
sunfeilong1993 <1498282352@qq.com>
2018-04-19 18:49:32 +0800

    同步代码块测试

---
+++ test/src/main/java/com/sun/xiaotian/demo/test/mutilthread/TestSynchronized.java
@@ -0,0 +1,28 @@
+package com.sun.xiaotian.demo.test.mutilthread;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class TestSynchronized {
+
+    public static void main(String[] args) {
+        InnerSynchronized innerSynchronized = new InnerSynchronized();
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                innerSynchronized.sayHello();
+            }).start();
+        }
+    }
+
+    public static class InnerSynchronized {
+        public synchronized void sayHello() {
+            System.out.println("Hello, Name");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
