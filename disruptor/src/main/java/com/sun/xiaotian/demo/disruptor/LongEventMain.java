2c954e387108baf5cb09b5f4864f995ef0e72e74
sunfeilong1993 <1498282352@qq.com>
2018-04-22 21:23:17 +0800

    完善

--- disruptor/src/main/java/com/sun/xiaotian/demo/disruptor/LongEventMain.java
+++ disruptor/src/main/java/com/sun/xiaotian/demo/disruptor/LongEventMain.java
@@ -1,7 +1,9 @@
 package com.sun.xiaotian.demo.disruptor;
 
+import com.lmax.disruptor.BlockingWaitStrategy;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
 
 import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
@@ -9,12 +11,15 @@ import java.util.concurrent.TimeUnit;
 public class LongEventMain {
 
     private static void handleEvent(LongEvent event, long sequence, boolean endOfBatch) {
-        System.out.println(endOfBatch);
         System.out.println("sequence:" + sequence + " " + event);
     }
 
+    private static void clearEventHandler(LongEvent event, long sequence, boolean endOfBatch) {
+        event.setValue(0);
+        System.out.println("clear: " + event);
+    }
+
     private static void translateOne(LongEvent event, long sequence, ByteBuffer buffer) {
-        System.out.println(sequence);
         event.setValue(buffer.getLong(0));
     }
 
@@ -23,15 +28,14 @@ public class LongEventMain {
         LongEventFactory logEventFactory = new LongEventFactory();
         LongEventThreadFactory longEventThreadFactory = new LongEventThreadFactory();
 
-        Disruptor<LongEvent> disruptor = new Disruptor<>(logEventFactory, bufferSize, longEventThreadFactory);
-        disruptor.handleEventsWith(LongEventMain::handleEvent);
+        Disruptor<LongEvent> disruptor = new Disruptor<>(logEventFactory, bufferSize, longEventThreadFactory, ProducerType.MULTI, new BlockingWaitStrategy());
+        disruptor.handleEventsWith(LongEventMain::handleEvent).then(LongEventMain::clearEventHandler);
         disruptor.start();
 
         RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
         ByteBuffer tempByteBuffer = ByteBuffer.allocate(8);
         for (long l = 0; l < 10000; l++) {
             tempByteBuffer.putLong(0, l);
-            ringBuffer.publishEvent(LongEventMain::translateOne, tempByteBuffer);
             ringBuffer.publishEvent(LongEventMain::translateOne, tempByteBuffer);
             TimeUnit.SECONDS.sleep(1);
         }
