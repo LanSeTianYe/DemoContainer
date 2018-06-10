d6ac8b3ddceafe675cba5872a5ff0dbb472f18e9
sunfeilong1993 <1498282352@qq.com>
2018-06-10 10:19:13 +0800

    kafka

---
+++ kafka/src/main/java/com/sun/xiaotian/demo/kafka/constant/KafkaConfigConstant.java
@@ -0,0 +1,6 @@
+package com.sun.xiaotian.demo.kafka.constant;
+
+
+public interface KafkaConfigConstant {
+    String BOOTSTRAP_SERVERS_CONFIG = "192.168.0.201:9092,192.168.0.202:9092,192.168.0.203:9092";
+}
