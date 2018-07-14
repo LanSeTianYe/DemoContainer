51f436c34356c1a932d77082ffefdd7f6eb33639
sunfeilong1993 <1498282352@qq.com>
2018-07-14 11:27:50 +0800

    解决 commmanLineRunner  同步运行问题

--- rabbitmq/src/main/java/com/sun/xiaotian/demo/rabbitmq/core/receiving/ReceivingMessage.java
+++ rabbitmq/src/main/java/com/sun/xiaotian/demo/rabbitmq/core/receiving/ReceivingMessage.java
@@ -3,9 +3,11 @@ package com.sun.xiaotian.demo.rabbitmq.core.receiving;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(3)
 public class ReceivingMessage {
 
     private final static Logger logger = LogManager.getLogger(ReceivingMessage.class);
@@ -18,5 +20,20 @@ public class ReceivingMessage {
     @RabbitListener(queues = "even_queue")
     public void processEvenNumber(String content) {
         logger.info("even number receive " +content + "...");
+    }
+
+    @RabbitListener(queues = "csdn_fans_1")
+    public void processCsdnFans1(String content) {
+        logger.info("csdn_fans_1 receive " +content + "...");
+    }
+
+    @RabbitListener(queues = "csdn_fans_2")
+    public void processCsdnFans2(String content) {
+        logger.info("csdn_fans_2 receive " +content + "...");
+    }
+
+    @RabbitListener(queues = "csdn_fans_3")
+    public void processCsdnFans3(String content) {
+        logger.info("csdn_fans_3 receive " +content + "...");
     }
 }
--- rabbitmq/src/main/java/com/sun/xiaotian/demo/rabbitmq/core/send/SendMessage.java
+++ rabbitmq/src/main/java/com/sun/xiaotian/demo/rabbitmq/core/send/SendMessage.java
@@ -1,30 +1,28 @@
 package com.sun.xiaotian.demo.rabbitmq.core.send;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 @Component
+@Order(2)
 public class SendMessage implements CommandLineRunner {
 
     private final static Logger logger = LogManager.getLogger(SendMessage.class);
 
-
     private Long index = 1L;
-
-    private final AmqpAdmin amqpAdmin;
 
     private final AmqpTemplate amqpTemplate;
 
     @Autowired
     public SendMessage(AmqpAdmin amqpAdmin, AmqpTemplate amqpTemplate) {
-        this.amqpAdmin = amqpAdmin;
         this.amqpTemplate = amqpTemplate;
         //初始化
         Exchange numberEx = ExchangeBuilder.directExchange("number_ex").build();
@@ -40,26 +38,31 @@ public class SendMessage implements CommandLineRunner {
         amqpAdmin.declareBinding(even);
     }
 
-
     @Override
     public void run(String... args) throws Exception {
-        while (true) {
-            try {
-                long start;
-                if (index % 2 == 0) {
-                    start = System.currentTimeMillis();
-                    amqpTemplate.convertAndSend("number_ex", "even", index);
-                    logger.info("even send cost time: " + (System.currentTimeMillis() - start));
-                } else {
-                    start = System.currentTimeMillis();
-                    amqpTemplate.convertAndSend("number_ex", "odd", index);
-                    logger.info("odd send cost time: " + (System.currentTimeMillis() - start));
+
+        CompletableFuture.runAsync(() -> {
+            while (true) {
+                try {
+                    long start;
+                    if (index % 2 == 0) {
+                        start = System.currentTimeMillis();
+                        amqpTemplate.convertAndSend("number_ex", "even", index);
+                        logger.info("even send cost time: " + (System.currentTimeMillis() - start));
+                    } else {
+                        start = System.currentTimeMillis();
+                        amqpTemplate.convertAndSend("number_ex", "odd", index);
+                        logger.info("odd send cost time: " + (System.currentTimeMillis() - start));
+                    }
+                    logger.info(String.format("send index: %s ...", index++));
+                    TimeUnit.MILLISECONDS.sleep(1000L);
+                    if (index == 1 << 10) {
+                        break;
+                    }
+                } catch (Exception e) {
+                    logger.error("send", e);
                 }
-                logger.info(String.format("send index: %s ...", index++));
-                TimeUnit.MILLISECONDS.sleep(100L);
-            } catch (Exception e) {
-                logger.error("send", e);
             }
-        }
+        });
     }
 }
