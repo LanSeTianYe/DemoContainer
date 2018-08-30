aaabbf9df7af5cd06c2e1fffde41e00c7d4a4e88
sunfeilong <sunfeilong1993@gmail.com>
2018-08-30 17:47:27 +0800

    IP和数字转换工具类

---
+++ test/src/main/java/com/sun/xiaotian/demo/test/util/IPAndNumberTranslateUtil.java
@@ -0,0 +1,18 @@
+package com.sun.xiaotian.demo.test.util;
+
+public class IPAndNumberTranslateUtil {
+
+    public static Long IpToNumber(String ipAddress) {
+        String[] ipNUmber = ipAddress.split("\\.");
+        return (Long.valueOf(ipNUmber[0]) << 24) + (Long.valueOf(ipNUmber[1]) << 16) + (Long.valueOf(ipNUmber[2]) << 8) + (Long.valueOf(ipNUmber[3]));
+    }
+
+    public static String numberToIp(Long ipNumber) {
+        String result = "";
+        result += (ipNumber >> 24) + ".";
+        result += ((ipNumber << (8 + 32)) >>> 56) + ".";
+        result += ((ipNumber << (16 + 32)) >>> 56) + ".";
+        result += ((ipNumber << (24 + 32)) >>> 56);
+        return result;
+    }
+}
---
+++ test/src/test/java/com/sun/xiaotian/demo/test/util/IPAndNumberTranslateUtilTest.java
@@ -0,0 +1,19 @@
+package com.sun.xiaotian.demo.test.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IPAndNumberTranslateUtilTest {
+
+    @Test
+    public void ipToNumber() {
+        String finalIp = "255.255.255.255";
+        Long maxIpNumber = IPAndNumberTranslateUtil.IpToNumber(finalIp);
+        for (Long i = 0L; i < maxIpNumber; i = i + 10000L) {
+            System.out.println(IPAndNumberTranslateUtil.numberToIp(i));
+            Assert.assertEquals(i, IPAndNumberTranslateUtil.IpToNumber(IPAndNumberTranslateUtil.numberToIp(i)));
+        }
+    }
+}
