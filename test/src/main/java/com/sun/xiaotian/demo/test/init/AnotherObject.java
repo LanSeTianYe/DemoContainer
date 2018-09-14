88e233288869b76f63e59c893f5892796130c578
sunfeilong <sunfeilong1993@gmail.com>
2018-09-14 12:04:12 +0800

    类初始化顺序

---
+++ test/src/main/java/com/sun/xiaotian/demo/test/init/AnotherObject.java
@@ -0,0 +1,15 @@
+package com.sun.xiaotian.demo.test.init;
+
+public class AnotherObject {
+
+    /**
+     * 静态代码块儿在类被虚拟机加载时会执行
+     */
+    static {
+        System.out.println("AnotherObject static code block");
+    }
+
+    public static void printName() {
+        System.out.println("AnotherObject ");
+    }
+}
