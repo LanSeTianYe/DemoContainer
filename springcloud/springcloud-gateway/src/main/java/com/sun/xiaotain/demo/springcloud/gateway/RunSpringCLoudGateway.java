7bc8e926dd1105dca65628c14ddb94dd48b2602b
sunfeilong <sunfeilong1993@gmail.com>
2018-09-21 15:48:36 +0800

    springcloud gateway

---
+++ springcloud/springcloud-gateway/src/main/java/com/sun/xiaotain/demo/springcloud/gateway/RunSpringCLoudGateway.java
@@ -0,0 +1,12 @@
+package com.sun.xiaotain.demo.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunSpringCLoudGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunSpringCLoudGateway.class, args);
+    }
+}
---
+++ springcloud/springcloud-gateway/src/main/java/com/sun/xiaotain/demo/springcloud/gateway/config/RouteConfig.java
@@ -0,0 +1,27 @@
+package com.sun.xiaotain.demo.springcloud.gateway.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    private String uri = "http://www.sunfeilong.com:8080";
+
+    private static final Logger logger = LogManager.getLogger(RouteConfig.class);
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(predicate -> predicate.host("www.sunfeilong.*")
+                        .filters(filter -> filter.addRequestHeader("source", "springcloud gateway"))
+                        .uri(uri)
+                ).build();
+
+    }
+
+}
