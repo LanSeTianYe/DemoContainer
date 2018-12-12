015d2874bac602c212d9443a96bd7a87a4021048
sunfeilong <sunfeilong1993@gmail.com>
2018-12-12 16:00:40 +0800

    完善

--- redis/src/main/java/com/sun/xiaotian/demo/redis/redis_cli/RedisHyperLogLogs.java
+++ redis/src/main/java/com/sun/xiaotian/demo/redis/redis_cli/RedisHyperLogLogs.java
@@ -3,13 +3,17 @@ package com.sun.xiaotian.demo.redis.redis_cli;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
+
 public class RedisHyperLogLogs {
 
     private static final JedisPool jedisPool = new JedisPool("192.168.0.201", 6379);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         try (Jedis redis = jedisPool.getResource()) {
+            DecimalFormat decimalFormat = new DecimalFormat("0.00");
             String set_key = "consumer_set";
             String hll_key = "consumer_hll";
             int count = 10000;
@@ -18,7 +22,7 @@ public class RedisHyperLogLogs {
                 String consumer = "consumer_" + i;
                 redis.sadd(set_key, consumer);
                 redis.pfadd(hll_key, consumer);
-                System.out.println("percent :");
+                System.out.println(String.format("percent: %s ", decimalFormat.format(i * 100.0 / count)));
             }
 
             System.out.println(redis.pfcount(hll_key));
