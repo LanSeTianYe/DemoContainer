7a667047b8b719938fec7e75f26cffd1df39519a
sunfeilong <sunfeilong1993@gmail.com>
2018-12-29 18:47:13 +0800

    547

---
+++ algorithm/src/main/java/com/sun/xiaotian/demo/algorithm/leetcode/FriendCircles_547.java
@@ -0,0 +1,14 @@
+package com.sun.xiaotian.demo.algorithm.leetcode;
+
+public class FriendCircles_547 {
+
+    public int findCircleNum(int[][] M) {
+        if (M.length <= 1) {
+            return M.length;
+        }
+
+        int[] friends = new int[M.length];
+
+        return 0;
+    }
+}
