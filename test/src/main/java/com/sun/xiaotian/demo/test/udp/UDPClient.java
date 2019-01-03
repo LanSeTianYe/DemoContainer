8be66ddb4af751abe83df241723312500cc2b580
sunfeilong <sunfeilong1993@gmail.com>
2019-01-03 18:47:38 +0800

    udp

---
+++ test/src/main/java/com/sun/xiaotian/demo/test/udp/UDPClient.java
@@ -0,0 +1,22 @@
+package com.sun.xiaotian.demo.test.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+/**
+ * UDP 客户端
+ */
+public class UDPClient {
+
+    public static void main(String[] args) throws IOException {
+        try (DatagramSocket datagramSocket = new DatagramSocket(10000)) {
+            byte[] message = new byte[1024];
+            DatagramPacket datagramPacket = new DatagramPacket(message, message.length);
+            while (true) {
+                datagramSocket.receive(datagramPacket);
+                System.out.println(String.format("accept: %s", new String(datagramPacket.getData())));
+            }
+        }
+    }
+}
---
+++ test/src/main/java/com/sun/xiaotian/demo/test/udp/UDPServer.java
@@ -0,0 +1,29 @@
+package com.sun.xiaotian.demo.test.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * UDP 服务器
+ */
+public class UDPServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        try (DatagramSocket datagramSocket = new DatagramSocket(11000)) {
+            long number = 0;
+            while (true) {
+                String message = String.valueOf(number);
+                System.out.println(String.format("send: %s", message));
+                //发送到 92.168.0.1~92.168.0.255 的所有机器
+                DatagramPacket datagramPacket = new DatagramPacket(message.getBytes(), message.length(), new InetSocketAddress("192.168.0.255", 10000));
+                datagramSocket.send(datagramPacket);
+                number++;
+                TimeUnit.SECONDS.sleep(1);
+            }
+        }
+    }
+}
