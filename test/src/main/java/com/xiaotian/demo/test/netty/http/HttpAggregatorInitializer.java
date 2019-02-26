45c3be95e80022ef83fe829f58f03d7c1f6e4329
sunfeilong <sunfeilong1993@gmail.com>
2019-02-26 17:32:30 +0800

    event

--- test/src/main/java/com/xiaotian/demo/test/netty/http/HttpAggregatorInitializer.java
+++ test/src/main/java/com/xiaotian/demo/test/netty/http/HttpAggregatorInitializer.java
@@ -4,6 +4,7 @@ import io.netty.channel.*;
 import io.netty.handler.codec.http.HttpClientCodec;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.timeout.IdleStateHandler;
 
 public class HttpAggregatorInitializer extends ChannelInitializer<Channel> {
 
@@ -21,6 +22,7 @@ public class HttpAggregatorInitializer extends ChannelInitializer<Channel> {
             pipeline.addLast("aggregator", new HttpObjectAggregator(521 * 2014));
             pipeline.addLast(new SendMessageHandler());
         } else {
+            pipeline.addLast("idleState", new IdleStateHandler(10, 5, 0));
             pipeline.addLast("codec", new HttpServerCodec());
             pipeline.addLast("aggregator", new HttpObjectAggregator(521 * 2014));
             pipeline.addLast(new ShowMessageHandler());
--- test/src/main/java/com/xiaotian/demo/test/netty/http/ShowMessageHandler.java
+++ test/src/main/java/com/xiaotian/demo/test/netty/http/ShowMessageHandler.java
@@ -3,13 +3,17 @@ package com.xiaotian.demo.test.netty.http;
 import com.alibaba.fastjson.JSON;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.timeout.IdleStateEvent;
 
 import java.nio.charset.StandardCharsets;
 
 public class ShowMessageHandler extends ChannelInboundHandlerAdapter {
+
+    private final static ByteBuf ping = Unpooled.copiedBuffer("ping", StandardCharsets.UTF_8);
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -27,5 +31,21 @@ public class ShowMessageHandler extends ChannelInboundHandlerAdapter {
                 HttpResponseStatus.OK,
                 byteBuf);
         ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //注释之后测试空闲事件
+        //ctx.close();
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            System.out.println(((IdleStateEvent) evt).state());
+            ctx.writeAndFlush(ping.duplicate()).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
     }
 }
