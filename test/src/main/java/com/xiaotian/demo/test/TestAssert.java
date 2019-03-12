514a37453bcac71064a58f2348392b91b2bbe625
sunfeilong <sunfeilong1993@gmail.com>
2019-03-12 14:15:16 +0800

    assert

---
+++ test/src/main/java/com/xiaotian/demo/test/TestAssert.java
@@ -0,0 +1,11 @@
+package com.xiaotian.demo.test;
+
+/**
+ * 开启断言虚拟机参数: -enableassertions
+ */
+public class TestAssert {
+
+    public static void main(String[] args) {
+        assert false : "always false";
+    }
+}
