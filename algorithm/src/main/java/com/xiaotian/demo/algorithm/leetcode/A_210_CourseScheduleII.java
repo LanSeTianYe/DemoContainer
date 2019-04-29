07a296dde3c28519ef3ce6bcf3b79021dfeec51c
sunfeilong <sunfeilong1993@gmail.com>
2019-04-29 17:51:25 +0800

    leetcode 210

--- algorithm/src/main/java/com/xiaotian/demo/algorithm/leetcode/A_210_CourseScheduleII.java
+++ algorithm/src/main/java/com/xiaotian/demo/algorithm/leetcode/A_210_CourseScheduleII.java
@@ -1,10 +1,14 @@
 package com.xiaotian.demo.algorithm.leetcode;
 
-import java.util.BitSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class A_210_CourseScheduleII {
 
+    private int courseIndex = 0;
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        courseIndex = 0;
         int[] result = new int[numCourses];
         if (null == prerequisites || prerequisites.length == 0) {
             for (int i = 0; i < numCourses; i++) {
@@ -13,36 +17,38 @@ public class A_210_CourseScheduleII {
             return result;
         }
 
-        BitSet[] courseDependence = new BitSet[numCourses];
-        BitSet remainCourse = new BitSet();
+        List<List<Integer>> dependency = new ArrayList<>();
 
-        for (int i = 0; i < numCourses; i++) {
-            remainCourse.set(i);
-            courseDependence[i] = new BitSet();
-        }
+        boolean[] hasTakes = new boolean[numCourses];
+        boolean[] inStacks = new boolean[numCourses];
+        int[] index = {0};
+
+        for (int i = 0; i < numCourses; i++)
+            dependency.add(new ArrayList<>());
 
         for (int[] prerequisite : prerequisites)
-            courseDependence[prerequisite[0]].set(prerequisite[1]);
+            dependency.get(prerequisite[0]).add(prerequisite[1]);
 
-        int nextCourse = getNextCourse(remainCourse, courseDependence);
-        int index = 0;
-        while (nextCourse >= 0) {
-            result[index++] = nextCourse;
-            remainCourse.set(nextCourse, false);
-            nextCourse = getNextCourse(remainCourse, courseDependence);
+        for (int i = 0; i < numCourses; i++) {
+            if (!take(i, dependency, hasTakes, inStacks, result)) return new int[]{};
         }
-        return remainCourse.cardinality() > 0 ? new int[]{} : result;
+        return result;
     }
 
-    private int getNextCourse(BitSet remainCourse, BitSet[] courseDependence) {
-        int nextIndex = remainCourse.nextSetBit(0);
-        while (nextIndex >= 0) {
-            courseDependence[nextIndex].and(remainCourse);
-            if (courseDependence[nextIndex].cardinality() == 0) {
-                return nextIndex;
+    private boolean take(int index, List<List<Integer>> dependency, boolean[] hasTake, boolean[] inStack, int[] result) {
+        if (hasTake[index]) return true;
+        if (inStack[index]) return false;
+
+        inStack[index] = true;
+        for (Integer subIndex : dependency.get(index)) {
+            boolean take = take(subIndex, dependency, hasTake, inStack, result);
+            if (!take) {
+                return false;
             }
-            nextIndex = remainCourse.nextSetBit(nextIndex + 1);
         }
-        return -1;
+        result[courseIndex++] = index;
+        inStack[index] = false;
+        hasTake[index] = true;
+        return true;
     }
 }
