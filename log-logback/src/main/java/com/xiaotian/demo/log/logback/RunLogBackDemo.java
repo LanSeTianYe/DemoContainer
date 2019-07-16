2439930dedfbc2dcb29f32826a805c59b9332eba
sunfeilong <sunfeilong1993@gmail.com>
2019-07-16 20:58:34 +0800

    logback

--- log-logback/src/main/java/com/xiaotian/demo/log/logback/RunLogBackDemo.java
+++ log-logback/src/main/java/com/xiaotian/demo/log/logback/RunLogBackDemo.java
@@ -1,6 +1,9 @@
 package com.xiaotian.demo.log.logback;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +13,8 @@ public class RunLogBackDemo {
 
     public static void main(String[] args) {
         log.info("start run application ... ...");
+//        LoggerContext loggerContext = ((LoggerContext) LoggerFactory.getILoggerFactory());
+//        StatusPrinter.print(loggerContext);
         SpringApplication.run(RunLogBackDemo.class, args);
     }
 }
