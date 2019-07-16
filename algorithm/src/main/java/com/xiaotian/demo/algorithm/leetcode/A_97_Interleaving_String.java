08f061fb4ae856c91c670d4eeb2b73eb49c3bdc5
sunfeilong <sunfeilong1993@gmail.com>
2019-07-16 20:58:59 +0800

    a_97

---
+++ algorithm/src/main/java/com/xiaotian/demo/algorithm/leetcode/A_97_Interleaving_String.java
@@ -0,0 +1,16 @@
+package com.xiaotian.demo.algorithm.leetcode;
+
+public class A_97_Interleaving_String {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1 == null && s2 == null && s3 == null) {
+            return true;
+        }
+        if (s3 == null) {
+            return false;
+        }
+
+
+        return true;
+    }
+}
