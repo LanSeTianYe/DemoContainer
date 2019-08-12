4b9f1b7dd61005cd786012d893c1d11b7f61e479
sunfielong <sunfeilong1993@gmail.com>
2019-08-12 20:41:04 +0800

    mybatis plus

---
+++ database/database-mybatis-plus-generate/src/main/java/com/xiaotian/demo/database/mybatis/plus/MybatisPlusGenerateStarter.java
@@ -0,0 +1,128 @@
+package com.xiaotian.demo.database.mybatis.plus;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MybatisPlusGenerateStarter {
+
+    private static final String PROJECT_PATH = "D:\\work\\hades\\hades\\hades-core\\";
+    private static final String PACKAGE_NAME = "com.egls.cashloan.hades.core";
+    public static final String MODEL = "sys";
+    private static final String TABLES = "arc_sys_menu,arc_sys_menu_perm,arc_sys_perm,arc_sys_role,arc_sys_role_menu,,arc_sys_role_perm,arc_sys_user,arc_sys_user_role";
+
+    public static void main(String[] args) {
+        AutoGenerator mpg = new AutoGenerator();
+        GlobalConfig config = config();
+        mpg.setGlobalConfig(config);
+        mpg.setDataSource(datasource());
+        PackageConfig packageInfo = packageConfig();
+        mpg.setPackageInfo(packageInfo);
+
+        String templatePath = "/templates/mapper.xml.ftl";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                String path = PROJECT_PATH + "/src/main/resources/mapper/" + packageInfo.getModuleName() + "/";
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return path + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+
+        InjectionConfig injectionConfig = injectionConfig();
+        injectionConfig.setFileOutConfigList(focList);
+//        injectionConfig.setFileCreate(new IFileCreate() {
+//            @Override
+//            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+//                //不创建 Controller 文件
+//                if (fileType == FileType.CONTROLLER) {
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
+        mpg.setCfg(injectionConfig);
+
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+//        strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+        strategy.setEntityLombokModel(true);
+
+        // 公共父类
+//        strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+        // 写于父类中的公共字段
+//        strategy.setSuperEntityColumns("id");
+        strategy.setInclude(TABLES.split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(packageInfo.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+    private static GlobalConfig config() {
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setOutputDir(PROJECT_PATH + "/src/main/java");
+        globalConfig.setAuthor("sunfielong");
+        globalConfig.setServiceName("%sService");
+        globalConfig.setServiceImplName("%sServiceImpl");
+        globalConfig.setFileOverride(false);
+        globalConfig.setDateType(DateType.TIME_PACK);
+        globalConfig.setOpen(false);
+        return globalConfig;
+    }
+
+    private static DataSourceConfig datasource() {
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUrl("jdbc:mysql://10.0.8.36:3306/cashloan_hades?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=UTC");
+        // dataSourceConfig.setSchemaName("public");
+        dataSourceConfig.setDriverName("com.mysql.jdbc.Driver");
+        dataSourceConfig.setUsername("root");
+        dataSourceConfig.setPassword("root");
+        return dataSourceConfig;
+    }
+
+    private static PackageConfig packageConfig() {
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setModuleName(MODEL);
+        packageConfig.setParent(PACKAGE_NAME);
+        return packageConfig;
+    }
+
+    private static InjectionConfig injectionConfig() {
+        return new InjectionConfig() {
+            @Override
+            public void initMap() {
+
+            }
+        };
+    }
+}
