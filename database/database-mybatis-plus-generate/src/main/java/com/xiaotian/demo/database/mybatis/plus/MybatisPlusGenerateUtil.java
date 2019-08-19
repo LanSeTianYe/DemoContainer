5076bd33e87cace6ef744c04c7a7f235cd32ff05
sunfielong <sunfeilong1993@gmail.com>
2019-08-19 16:32:08 +0800

    mybatis generate

--- database/database-mybatis-plus-generate/src/main/java/com/xiaotian/demo/database/mybatis/plus/MybatisPlusGenerateUtil.java
+++ database/database-mybatis-plus-generate/src/main/java/com/xiaotian/demo/database/mybatis/plus/MybatisPlusGenerateUtil.java
@@ -13,14 +13,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MybatisPlusGenerateStarter {
-
-    private static final String PROJECT_PATH = "D:\\work\\hades\\hades\\hades-core\\";
-    private static final String PACKAGE_NAME = "com.egls.cashloan.hades.core";
-    public static final String MODEL = "sys";
-    private static final String TABLES = "arc_sys_menu,arc_sys_menu_perm,arc_sys_perm,arc_sys_role,arc_sys_role_menu,,arc_sys_role_perm,arc_sys_user,arc_sys_user_role";
+/**
+ * Mybatis 代码生成公工具
+ *
+ * @author sunfeilong   (sunfeilong@eglsgame.com)
+ * @version V1.0
+ * @date 2019/8/17 13:38
+ */
+public class MybatisPlusGenerateUtil {
 
     public static void main(String[] args) {
+        //初始化 core 模块代码
+        //Config core = Config.CORE;
+        //core.setTables("");
+        //generate(Config.CORE);
+        //初始化 cr 模块代码
+        //generate(Config.CR);
+    }
+
+
+    public enum Config {
+
+        /**
+         * Core 模块配置
+         */
+        CORE("D:\\work\\hades\\hades\\hades-core\\", "com.egls.cashloan.hades", "core", "arc_sys_menu,arc_sys_menu_perm,arc_sys_perm,arc_sys_role,arc_sys_role_menu,,arc_sys_role_perm,arc_sys_user,arc_sys_user_role,arc_sys_dict,arc_sys_dict_detail"),
+        CR("D:\\work\\hades\\hades\\hades-cr\\", "com.egls.cashloan.hades", "cr", "cl_user_rule,cr_scene_business,cr_scene_business_log,cr_third_credit_req_log,cr_tpp,cr_tpp_business"),
+        ;
+
+        /**
+         * 项目路径
+         */
+        private final String projectPath;
+        /**
+         * 项目包名
+         */
+        private final String packageName;
+
+        /**
+         * 模块儿名
+         */
+        private final String modelName;
+        /**
+         * 数据库表明，多个用逗号分隔
+         */
+        private String tables;
+
+        Config(String projectPath, String packageName, String modelName, String tables) {
+            this.projectPath = projectPath;
+            this.packageName = packageName;
+            this.modelName = modelName;
+            this.tables = tables;
+        }
+
+
+        /**
+         * 设置生成的表，多表以逗号分隔
+         *
+         * @param tables 表，多个用逗号分隔
+         */
+        public void setTables(String tables) {
+            this.tables = tables;
+        }
+    }
+
+    /**
+     * 根据配置生成Mapper等文件
+     *
+     * @param generateConfig 初始化配置
+     */
+    public static void generate(Config generateConfig) {
         AutoGenerator mpg = new AutoGenerator();
         GlobalConfig config = config();
         mpg.setGlobalConfig(config);
@@ -36,7 +98,7 @@ public class MybatisPlusGenerateStarter {
         focList.add(new FileOutConfig(templatePath) {
             @Override
             public String outputFile(TableInfo tableInfo) {
-                String path = PROJECT_PATH + "/src/main/resources/mapper/" + packageInfo.getModuleName() + "/";
+                String path = generateConfig.projectPath + "/src/main/resources/mapper/" + packageInfo.getModuleName() + "/";
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
                 return path + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
@@ -80,7 +142,7 @@ public class MybatisPlusGenerateStarter {
 //        strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
         // 写于父类中的公共字段
 //        strategy.setSuperEntityColumns("id");
-        strategy.setInclude(TABLES.split(","));
+        strategy.setInclude(generateConfig.tables.split(","));
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(packageInfo.getModuleName() + "_");
         mpg.setStrategy(strategy);
@@ -88,9 +150,9 @@ public class MybatisPlusGenerateStarter {
         mpg.execute();
     }
 
-    private static GlobalConfig config() {
+    private static GlobalConfig config(Config generateConfig) {
         GlobalConfig globalConfig = new GlobalConfig();
-        globalConfig.setOutputDir(PROJECT_PATH + "/src/main/java");
+        globalConfig.setOutputDir(generateConfig.projectPath + "/src/main/java");
         globalConfig.setAuthor("sunfielong");
         globalConfig.setServiceName("%sService");
         globalConfig.setServiceImplName("%sServiceImpl");
@@ -106,14 +168,14 @@ public class MybatisPlusGenerateStarter {
         // dataSourceConfig.setSchemaName("public");
         dataSourceConfig.setDriverName("com.mysql.jdbc.Driver");
         dataSourceConfig.setUsername("root");
-        dataSourceConfig.setPassword("root");
+        dataSourceConfig.setPassword("tuhOSw2Cdg)W");
         return dataSourceConfig;
     }
 
-    private static PackageConfig packageConfig() {
+    private static PackageConfig packageConfig(Config generateConfig) {
         PackageConfig packageConfig = new PackageConfig();
-        packageConfig.setModuleName(MODEL);
-        packageConfig.setParent(PACKAGE_NAME);
+        packageConfig.setModuleName(generateConfig.modelName);
+        packageConfig.setParent(generateConfig.packageName);
         return packageConfig;
     }
 
