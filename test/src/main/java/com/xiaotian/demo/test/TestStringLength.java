722729e49838388940d1cc5aa95a11ae330acba8
sunfielong <sunfeilong1993@gmail.com>
2019-08-27 12:11:48 +0800

    字符串字节数

---
+++ test/src/main/java/com/xiaotian/demo/test/TestStringLength.java
@@ -0,0 +1,10 @@
+package com.xiaotian.demo.test;
+
+public class TestStringLength {
+
+    public static void main(String[] args) {
+        String name = "123abc张三";
+        System.out.println(name.length());
+        System.out.println(name.getBytes().length);
+    }
+}
