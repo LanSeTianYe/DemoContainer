6be76219f738e42033bf04673f1b7b2e429658b5
sunfeilong <sunfeilong1993@gmail.com>
2019-12-13 11:13:29 +0800

    stream

--- test/src/main/java/com/xiaotian/demo/test/TestStream.java
+++ test/src/main/java/com/xiaotian/demo/test/TestStream.java
@@ -1,14 +1,32 @@
 package com.xiaotian.demo.test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 public class TestStream {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3);
-        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
-        System.out.println(sum);
+        Random random = new Random();
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < 10000000; i++) {
+            numbers.add(random.nextInt(10000));
+        }
+
+        //所有元素的和
+        long start1 = System.currentTimeMillis();
+        Optional<Integer> reduce = numbers.stream().reduce(Integer::sum);
+        System.out.println(reduce);
+        long end1 = System.currentTimeMillis();
+        System.out.println("cost time: " + (end1 - start1));
+
+        long start = System.currentTimeMillis();
+        Optional<Integer> reduce2 = numbers.parallelStream().reduce(Integer::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(reduce2);
+        System.out.println("cost time: " + (end - start));
     }
-    
+
 }
