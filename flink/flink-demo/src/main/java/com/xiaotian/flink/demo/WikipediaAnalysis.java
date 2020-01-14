397f51e607f61b62b9779cbf1a02d793ae57dc89
sunfielong <sunfeilong1993@gmail.com>
2020-01-14 16:11:42 +0800

    代码调整

--- flink/flink-demo/src/main/java/com/xiaotian/flink/demo/WikipediaAnalysis.java
+++ flink/flink-demo/src/main/java/com/xiaotian/flink/demo/WikipediaAnalysis.java
@@ -2,9 +2,6 @@ package com.xiaotian.flink.demo;
 
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
@@ -13,12 +10,14 @@ import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
 public class WikipediaAnalysis {
 
     public static void main(String[] args) throws Exception {
+
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStreamSource<WikipediaEditEvent> streamSource = executionEnvironment.addSource(new WikipediaEditsSource());
-        KeyedStream<WikipediaEditEvent, String> keyedStream = streamSource.keyBy(WikipediaEditEvent::getUser);
-        SingleOutputStreamOperator<Tuple2<String, Long>> result = keyedStream
+
+        executionEnvironment.addSource(new WikipediaEditsSource())
+                .keyBy(WikipediaEditEvent::getUser)
                 .timeWindow(Time.seconds(5))
                 .aggregate(new AggregateFunction<WikipediaEditEvent, Tuple2<String, Long>, Tuple2<String, Long>>() {
+
                     private static final long serialVersionUID = -184305568733432800L;
 
                     @Override
@@ -42,9 +41,8 @@ public class WikipediaAnalysis {
                     public Tuple2<String, Long> merge(Tuple2<String, Long> a, Tuple2<String, Long> b) {
                         return new Tuple2<>(a.f0, a.f1 + b.f1);
                     }
-                });
-
-        result.print();
+                })
+                .print();
         executionEnvironment.execute();
     }
 }
